f2b680b06fcb76cce28c1cdc8aecdd4ae63919ae
Jeffrey Boudreaux <jaboudreauxjr@gmail.com>
2019-12-08 21:15:01 -0600

    Currently working on Section 4 part 8

--- app/src/main/java/com/example/xu_map/GraphMap.java
+++ app/src/main/java/com/example/xu_map/GraphMap.java
@@ -23,55 +23,48 @@ public class GraphMap {
     }
 
     public GraphMap(){
-        new HashMap<>();
+        XU_Map = new HashMap<>();
     }
 
     public void addVertex(Location loc){
-        try{
-            XU_Map.put(loc, new LinkedList<Edge>());
-        }catch (NullPointerException e){
-           // Log.d("MainActivity", "It did not work");
-        }
+        XU_Map.put(loc, new LinkedList<Edge>());
+
     }
 
     public void addEdge(Location source, Location destination, int weight, boolean bidirectional){
 
         Edge edge = new Edge(source, destination, weight);
-        try{
-            if (!XU_Map.containsKey(source)){
-                addVertex(source);
-            }
 
-            if (!XU_Map.containsKey(destination)){
-                addVertex(destination);
-            }
-
-            XU_Map.get(source).add(edge);
-            Log.d("MainActivity", String.valueOf(XU_Map.get(source.getBuildName()).getFirst()));
-
-            if (bidirectional == true) {
-                XU_Map.get(destination).add(edge);
-            }
-        }catch (NullPointerException e){
-
+        if (!XU_Map.containsKey(source)){
+            addVertex(source);
         }
+
+        if (!XU_Map.containsKey(destination)){
+            addVertex(destination);
+        }
+
+        XU_Map.get(source).add(edge);
+
+        if (bidirectional == true) {
+            XU_Map.get(destination).add(edge);
+        }
+
 
     }
 
     public LinkedList<Edge> getEdges(Location source){
-        try{
-            return XU_Map.get(source);
-        }catch (NullPointerException e){
-            return null;
-        }
+
+        return XU_Map.get(source);
+
 
     }
     public Set<Location> getVertices(){
-        try{
-            return XU_Map.keySet();
-        }catch (NullPointerException e){
-            return null;
-        }
+        return XU_Map.keySet();
+
+    }
+
+    public void OptimalPath(){
+
     }
 
     @Override
@@ -83,7 +76,7 @@ public class GraphMap {
             for (Location v : XU_Map.keySet()) {
                 builder.append(v.getBuildName() + ": ");
                 for (Edge w : XU_Map.get(v)) {
-                    builder.append(w.toString() + " ");
+                    builder.append(w.destination.getBuildName() + " ");
                 }
                 builder.append("\n");
             }
--- app/src/main/java/com/example/xu_map/MainActivity.java
+++ app/src/main/java/com/example/xu_map/MainActivity.java
@@ -31,17 +31,16 @@ public class MainActivity extends AppCompatActivity {
 
         for (Map.Entry<String,Location> entry : obj.newObjects.entrySet()){
             gm.addVertex(entry.getValue());
-            //Log.d(TAG, String.valueOf(entry.getKey()));
         }
 
-        gm.addEdge(obj.getLocation("NCF Addition"), obj.getLocation("NCF Science Annex"), 5, true);
-        gm.addEdge(obj.getLocation("NCF Addition"), obj.getLocation("NCF Science Annex"), 4, true);
-        gm.addEdge(obj.getLocation("NCF Addition"), obj.getLocation("NCF Science Annex"), 6, true);
-        gm.addEdge(obj.getLocation("NCF Addition"), obj.getLocation("NCF Science Annex"), 3, true);
-        gm.addEdge(obj.getLocation("NCF Addition"), obj.getLocation("NCF Science Annex"), 2, true);
-        gm.addEdge(obj.getLocation("NCF Addition"), obj.getLocation("NCF Science Annex"), 7, true);
+        gm.addEdge(obj.getLocation("NCF Addition"), obj.getLocation("Music Building"), 5, true);
+        gm.addEdge(obj.getLocation("Administration Building"), obj.getLocation("NCF Addition"), 4, true);
+        gm.addEdge(obj.getLocation("Pharmacy Addition"), obj.getLocation("Administration Building"), 6, true);
+        gm.addEdge(obj.getLocation("University Center"), obj.getLocation("Pharmacy Addition"), 3, true);
+        gm.addEdge(obj.getLocation("College of Pharmacy"), obj.getLocation("University Center"), 2, true);
+        gm.addEdge(obj.getLocation("Music Building"), obj.getLocation("College of Pharmacy"), 7, true);
 
-        Log.d(TAG, String.valueOf(gm.getEdges(obj.getLocation("NCF Addition"))));
+        Log.d(TAG, gm.toString());
     }
 
 }
